c179dc806ca1921b9384b00eb130e32eb0ca044a
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-23 15:25:20 -0500

    Update whileLoop.cs

--- whileLoop.cs
+++ whileLoop.cs
@@ -3,12 +3,12 @@ using System;
 
 class MainClass {
     public static void Main(string[] args) {
-        Console.WriteLine("What are 5 things you want to do?");
+        Console.WriteLine("What are 5 activities you want to do?");
         string response = Console.ReadLine();
         int count = 0;
         while (response != "sleep" && response != "nothing" && count != 4) {
-            // Process the data.
-            Console.WriteLine("Thing {0}?", ((count + 1)+1));
+            // Continue asking up to 5 activities unless the respond with sleep or nothing as an activity
+            Console.WriteLine("Activity {0}?", ((count + 1)+1));
             response = Console.ReadLine();
             count++;
         }

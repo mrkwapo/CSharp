f8e03041ad014c84a2b21dfd9ba9fccdc615fbfe
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-23 15:19:58 -0500

    Create whileLoop.cs

---
+++ whileLoop.cs
@@ -0,0 +1,16 @@
+//This program was created to practice the while loop
+using System;
+
+class MainClass {
+    public static void Main(string[] args) {
+        Console.WriteLine("What are 5 things you want to do?");
+        string response = Console.ReadLine();
+        int count = 0;
+        while (response != "sleep" && response != "nothing" && count != 4) {
+            // Process the data.
+            Console.WriteLine("Thing {0}?", ((count + 1)+1));
+            response = Console.ReadLine();
+            count++;
+        }
+    }
+}

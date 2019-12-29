16b2160a2e248861770890d1021170507838c95d
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-29 16:22:37 -0500

    Create admissionsForm2.cs

---
+++ admissionsForm2.cs
@@ -0,0 +1,71 @@
+//This program demonstrates getting and printing information from a user using 2 methods called in the main class 
+using System;
+
+namespace studentInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student1 = new Student();
+
+            student1.AddStudentInfo();
+            student1.AddTeacherInfo();
+            Student.PrintInfo();
+
+        }                  
+        
+
+        //Class or Blueprint
+        public class Student
+        {
+            //Constructor
+            public Student (){
+                }
+
+            //Method
+            public void AddStudentInfo()
+            {
+                Console.WriteLine("First name: ");
+                firstName = Console.ReadLine();
+
+                Console.WriteLine("Last name: ");
+                lastName = Console.ReadLine();
+
+                Console.WriteLine("Birthday: ");
+                birthday = Console.ReadLine();
+
+            }
+
+            //Method
+            public void AddTeacherInfo()
+            {
+                Console.WriteLine("Teacher name: ");
+                teacherName = Console.ReadLine();
+
+                Console.WriteLine("Course: ");
+                course = Console.ReadLine();
+
+                Console.WriteLine("Degree: ");
+                degree = Console.ReadLine();
+
+            }
+
+            //Method
+            public static void PrintInfo()
+            {
+                //Output to the console window               
+                Console.WriteLine("{0} major {1} {2} was born {3} and has a {4} course with {5}", degree, firstName, lastName, birthday, course, teacherName);
+
+            }
+            public static string firstName;
+            public static string lastName;
+            public static string birthday;
+            public static string teacherName;
+            public static string course;
+            public static string degree;
+        }
+
+        
+    }
+}

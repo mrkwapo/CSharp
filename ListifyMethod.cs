df2aa5c2c4fe9d5bc6e94ed93259af65b558b049
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-01-06 17:07:26 -0500

    Create ListifyMethod.cs

---
+++ ListifyMethod.cs
@@ -0,0 +1,30 @@
+//Turn a string of numbers hyphenated by numbers into a List containing each number
+using System;
+using System.Collections.Generic;
+
+class MainClass {  
+
+  public static void Main (string[] args) {
+    Console.WriteLine ("Supply a list of hyphen separated numbers (e.g 5-6-7-8-9):");
+    var input = Console.ReadLine();  
+
+      foreach(var number in Listify(input))
+      Console.WriteLine(number);
+    
+  }
+
+  public static List<int> Listify(string input)
+  {
+    string[] series = input.Split('-');
+
+    var numbers = new List<int>();
+
+    foreach(var number in series)
+    {
+      numbers.Add(Convert.ToInt32(number));
+    }
+    numbers.Sort();
+    return numbers;
+
+  }
+}

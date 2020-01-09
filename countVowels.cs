be2de9f02f681f60b793ed556148bcb0d19ab22a
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-01-09 00:30:13 -0500

    Create countVowels.cs

---
+++ countVowels.cs
@@ -0,0 +1,45 @@
+/*Challenge: Write a program and ask the user to enter an English word. Count the number of vowels (a, e, o, u, i) in the word. 
+ * So, if the user enters "inadequate", the program should display 6 on the console. */
+using System;
+
+namespace CountVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word: ");
+            var input = Console.ReadLine();
+            
+            if(String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error. You must enter a word!");
+                return;
+            }            
+            
+            Console.WriteLine("This word contains {0} vowels.", CountVowels(input));
+        }
+
+        public static int CountVowels(string input)
+        {
+            var count = 0;
+
+            foreach (var letter in input)
+            {
+                switch (letter)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                    case 'y':
+                        count++;
+                        break;
+                }
+            }
+
+            return count;
+        }
+    }
+}

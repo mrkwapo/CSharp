9d620577efe012ba634111f1cd1fc959da152436
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-02-05 19:17:41 -0500

    Update separateEvenAndOdd.cs

--- hackerRank/separateEvenAndOdd.cs
+++ hackerRank/separateEvenAndOdd.cs
@@ -1,5 +1,5 @@
 /*Given a string, S, of length N  that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2 space-separated strings 
-on a single line (see the Sample below for more detail).*/
+on a single line (See the Sample below for more detail).*/
 
 using System;
 using System.Collections.Generic;
